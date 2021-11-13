c168677e74f3f831a8929f91feb72f81edd564f5
Istiak98 <istiakahmed104127@gmail.com>
2021-11-13 23:14:24 +0600

    1Up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./componets/Home/Home/Home";
+import Navbar from "./componets/Shared/Navbar/Navbar";
+import Footer from "./componets/Shared/Footer/Footer";
+import Aboutus from "./componets/Home/Aboutus/Aboutus";
+import ContactUs from "./componets/Home/Contactus/Contactus";
+import NotFound from "./componets/Shared/NotFound/NotFound";
+import AllDrones from "./componets/Home/AllDrones/AllDrones";
+// import Login from "./componets/Shared/Login/Login";
+import AuthProvider from "./context/AuthProvider";
+import PlaceOrder from "./componets/Home/PlaceOrder/PlaceOrder";
+import PrivateRoute from "./componets/Shared/Login/PrivateRoute/PrivateRoute";
+import Dashbaord from "./componets/Home/Dashboard/Dashboard";
+import LoginForm from "./componets/Home/LoginFrom/LoginFrom";
+import RegistrationForm from "./componets/Home/RegistrationFrom/RegistrationFrom";
+import MyOrders from "./componets/Home/MyOrders/MyOrders";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Router>
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/alldrones">
+              <AllDrones></AllDrones>
+            </Route>
+
+            <Route path="/aboutus">
+              <Aboutus></Aboutus>
+            </Route>
+
+            <Route path="/contactus">
+              <ContactUs></ContactUs>
+            </Route>
+            {/* <Route path="/login">
+              <Login></Login>
+            </Route> */}
+            <Route path="/loginform">
+              <LoginForm></LoginForm>
+            </Route>
+            <Route path="/registrationform">
+            <RegistrationForm></RegistrationForm>
+            </Route>
+            <PrivateRoute path="/dashboard">
+              <Dashbaord></Dashbaord>
+            </PrivateRoute>
+            <PrivateRoute path="/placeOrder/:droneId">
+              <PlaceOrder></PlaceOrder>
+            </PrivateRoute>
+            <PrivateRoute  path="/dashboard/myorders">
+              <MyOrders></MyOrders>
+            </PrivateRoute>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          {/* <Footer></Footer> */}
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
